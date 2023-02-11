6c564caca009d4a799e35e4c051687b1ecaba998
Ekrem Sönmezer <ekrem.sonmezer@picussecurity.com>
2023-02-12 01:32:06 +0300

    change scripts

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,27 @@
-import {quotes} from "./quotes.js";
-import {about} from "./about.js";
-import * as languages from "./languages.js";
+import {about, months, quotes, shortenWeekdays, weekdays} from "./data.js";
 
-let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-let weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-let weekdaysShort = ["Mon", "Tue", " Wed", "Thu", "Fri", "Sat", "Sun"];
-let dateInterval;
-let timeInterval;
-
+let dateInterval, timeInterval;
 let preferences = {
+    version: 2,
+    weather: {
+        show: true,
+        temperature: null,
+        text: null,
+        icon: null,
+        lastRefresh: null
+    },
+    background: {
+        query: "nature",
+        src: null,
+        link: null,
+        user: null,
+        lastRefresh: null
+    },
     showTime: true,
     showDate: true,
     dateFormat: "d-m-w",
-    imageLastRefresh: null,
     font: "Prompt",
-    image: null,
     showCalendar: true,
-    showWeather: true,
-    weatherLastRefresh: null,
-    weather: null,
-    weatherIcon: null,
     location: "London",
     darkBackground: false,
     showQuotes: true,
@@ -29,18 +31,18 @@ let preferences = {
 
 $("#prev-month").click(() => {
     const current = $("#month-name").text();
-    if (current === months[new Date().getMonth()]) {
+    if (current === months[preferences.language][new Date().getMonth()]) {
         showCalendar(-1);
-    } else if (current === months[new Date().getMonth() + 1]) {
+    } else if (current === months[preferences.language][new Date().getMonth() + 1]) {
         showCalendar(0);
     }
 });
 
 $("#next-month").click(() => {
     const current = $("#month-name").text();
-    if (current === months[new Date().getMonth()]) {
+    if (current === months[preferences.language][new Date().getMonth()]) {
         showCalendar(1);
-    } else if (current === months[new Date().getMonth() - 1]) {
+    } else if (current === months[preferences.language][new Date().getMonth() - 1]) {
         showCalendar(0);
     }
 });
@@ -54,11 +56,7 @@ $("#calendar-btn").click(() => {
 });
 
 $("#refresh-btn").click(() => {
-    preferences.image = `https://picsum.photos/seed/${Math.floor(Math.random() * 100000000)}/1920/1080`;
-    preferences.lastRefresh = Date.now();
-    saveToStorage();
-    $("body").css("background", `linear-gradient(to bottom, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), url(${preferences.image}) no-repeat center center fixed`);
-    $("#attribution").html(`Photo on <a href=${preferences.image}>Lorem Picsum</a>`);
+    setBackground(true);
 });
 
 $("#show-about").click(() => {
@@ -87,7 +85,7 @@ $('#toggle-date').change(() => {
 
 $('#show-weather').change(() => {
     const value = $('#show-weather').prop("checked");
-    preferences.showWeather = value;
+    preferences.weather.show = value;
     if (value) {
         $("#weather-container").show();
         $("#location").prop("disabled", false);
@@ -141,7 +139,7 @@ $('#dark-background').change(() => {
     if (value) {
         $("#body").css("background", "#212121");
     } else {
-        $("#body").css("background", `linear-gradient(to bottom, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(${preferences.image}) no-repeat center center fixed`);
+        setBackground();
     }
     saveToStorage();
 });
@@ -154,10 +152,9 @@ $("#date-order-select").change(() => {
 });
 
 $("#language-select").change(() => {
-    const value = $("#language-select option:selected").val();
-    preferences.language = value;
+    preferences.language = $("#language-select option:selected").val();
     saveToStorage();
-    changeLanguage(value);
+    changeLanguage();
 });
 
 $("#font-select").change(() => {
@@ -172,13 +169,24 @@ $("#location").on('keyup', function (e) {
         let value = $("#location").val();
         value = value.replace(/\s/, ",");
         preferences.location = value;
-        preferences.weather = null;
-        showWeather(preferences.language);
+        preferences.weather = {
+            show: true,
+            temperature: null,
+            text: null,
+            icon: null,
+            lastRefresh: null
+        };
+        showWeather();
     }
 });
 
-$("#weather-container").click(function () {
-    window.location = preferences.weather == null ? "https://openweathermap.org" : `https://openweathermap.org/city/${preferences.weather[2]}`;
+$("#background-query").on('keyup', function (e) {
+    if (e.key === 'Enter' || e.keyCode === 13) {
+        let value = $("#background-query").val();
+        value = value.replace(/\s/, ",");
+        preferences.background.query = value;
+        setBackground(true);
+    }
 });
 
 $(document).on('click', '.main-folder', function () {
@@ -192,7 +200,6 @@ $(document).mouseup(function (e) {
     if (!container.is(e.target) && container.has(e.target).length === 0) {
         container.hide();
     }
-
 });
 
 
@@ -204,36 +211,25 @@ function saveToStorage() {
 
 function loadFromStorage() {
     chrome.storage.sync.get('preferences', function (result) {
-        if (result.preferences == null) {
+        if (result.preferences == null || result.preferences.version == null || result.preferences < 2) {
             saveToStorage();
             loadFromStorage();
         } else {
             preferences = result.preferences;
 
-            months = languages["months" + preferences.language];
-            weekdays = languages["weekdays" + preferences.language];
-            weekdaysShort = languages["weekdaysShort" + preferences.language];
-
             $("*").css("font-family", preferences.font);
-
-            if ((Math.abs(Date.now() - preferences.lastRefresh) / 36e5) > 24 || preferences.image == null) {
-                preferences.image = `https://picsum.photos/seed/${Math.floor(Math.random() * 100000000)}/1920/1080`;
-                preferences.lastRefresh = Date.now();
-                saveToStorage();
-            }
 
             if (preferences.darkBackground) {
                 $("#dark-background").prop("checked", "checked");
                 $("#body").css("background", "#212121");
             } else {
                 $("#dark-background").prop("checked", false);
-                $("body").css("background", `linear-gradient(to bottom, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(${preferences.image}) no-repeat center center fixed`);
+                setBackground();
             }
-
-            $("#attribution").html(`Photo on <a href=${preferences.image}>Lorem Picsum</a>`);
 
             $("#date-order-select").val(preferences.dateFormat);
             $("#location").val(preferences.location);
+            $("#background-query").val(preferences.background.query);
             $("#language-select").val(preferences.language);
             $("#font-select").val(preferences.font);
 
@@ -246,10 +242,10 @@ function loadFromStorage() {
                 $("#date-order-select").prop("disabled", "disabled");
             }
 
-            if (preferences.showWeather) {
+            if (preferences.weather.show) {
                 $("#show-weather").prop("checked", "checked");
                 $("#location").prop("disabled", false);
-                showWeather(preferences.language);
+                showWeather();
             } else {
                 $("#show-weather").prop("checked", false);
                 $("#location").prop("disabled", "disabled");
@@ -298,13 +294,13 @@ function showDate() {
                     value.push(today.getDate());
                     break;
                 case "m":
-                    value.push(months[today.getMonth()]);
+                    value.push(months[preferences.language][today.getMonth()]);
                     break;
                 case "y":
                     value.push(today.getFullYear());
                     break;
                 case "w":
-                    value.push(weekdays[today.getDay()]);
+                    value.push(weekdays[preferences.language][today.getDay()]);
                     break;
             }
         }
@@ -331,7 +327,7 @@ function showCalendar(monthShift) {
     for (let i = 0; i < 7; i++) {
         const span = $("<span></span>");
         span.addClass("weekday");
-        span.text(weekdaysShort[i].substr(0, 3));
+        span.text(shortenWeekdays[preferences.language][i].substr(0, 3));
         $("#weekdays").append(span);
     }
 
@@ -361,38 +357,73 @@ function showCalendar(monthShift) {
         div.appendTo(divContainer);
         weekday = 0;
     }
-    $("#month-name").text(months[today.getMonth() + monthShift]);
+    $("#month-name").text(months[preferences.language][today.getMonth() + monthShift]);
 }
 
-function showWeather(language, force = false) {
+function showWeather(force = false) {
     if (preferences.location == null || preferences.location === "")
         return;
-    if (preferences.weather == null || (Math.abs(Date.now() - preferences.weatherLastRefresh) / 36e5) > 1 || force) {
-        fetch(`https://minitime.netlify.app/.netlify/functions/getWeather?location=${preferences.location}&language=${language.toLowerCase()}`,
+    if (preferences.weather.lastRefresh == null || (Math.abs(Date.now() - preferences.weather.lastRefresh) / 36e5) > 1 || force) {
+        fetch(`https://nwv1k1ugmf.execute-api.eu-central-1.amazonaws.com/Prod/weather?operation=current&q=${preferences.location}&language=${preferences.language.toLowerCase()}`,
             {
                 method: 'get',
                 headers: {'Content-Type': 'application/json'}
             })
             .then(response => response.json())
             .then(data => {
-                preferences.weather = [data.weather[0].description, Math.round(data.main.temp).toString() + "°C", data.id];
-                preferences.weatherIcon = `../images/openweather/${data.weather[0].icon}.png`;
-                $("#weather-city").text(toTitleCase(preferences.location.replace(/,/g, " ")));
-                $("#weather-status").text(preferences.weather[0]);
-                $("#weather-temp").text(preferences.weather[1]);
-                $("#weather-icon").attr("src", preferences.weatherIcon);
-                preferences.weatherLastRefresh = Date.now();
-                $("#weather-container").attr("title", `Powered by OpenWeather, Last Refresh: ${new Date(preferences.weatherLastRefresh).toLocaleTimeString("en-GB").substr(0, 5)}`);
+                preferences.weather = {
+                    show: true,
+                    temperature: data.current.temp_c.toString() + "°C",
+                    text: data.current.condition.text,
+                    icon: "http://" + data.current.condition.icon.replace("//", ""),
+                    lastRefresh: Date.now()
+                };
+                setWeatherValuesToHtml();
                 saveToStorage();
             })
             .catch(error => console.error(error));
     } else {
-        $("#weather-city").text(toTitleCase(preferences.location.replace(/,/g, " ")));
-        $("#weather-status").text(preferences.weather[0]);
-        $("#weather-temp").text(preferences.weather[1]);
-        $("#weather-icon").attr("src", preferences.weatherIcon);
-        $("#weather-container").attr("title", `Powered by OpenWeather, Last Refresh: ${new Date(preferences.weatherLastRefresh).toLocaleTimeString("en-GB").substr(0, 5)}`);
+        setWeatherValuesToHtml();
     }
+}
+
+function setBackground(force = false) {
+    if ((Math.abs(Date.now() - preferences.background.lastRefresh) / 36e5) > 24 || preferences.background.src == null || force) {
+        console.log(preferences.background);
+        fetch(`https://nwv1k1ugmf.execute-api.eu-central-1.amazonaws.com/Prod/background?query=${encodeURIComponent(preferences.background.query)}`,
+            {
+                method: 'get',
+                headers: {'Content-Type': 'application/json'}
+            })
+            .then(response => response.json())
+            .then(data => {
+                preferences.background = {
+                    query: preferences.background.query,
+                    src: data.raw_url,
+                    link: data.link,
+                    user: data.user,
+                    lastRefresh: Date.now()
+                };
+                setBackgroundValuesToHtml();
+                saveToStorage();
+            })
+            .catch(error => console.error(error));
+    } else {
+        setBackgroundValuesToHtml();
+    }
+}
+
+function setWeatherValuesToHtml() {
+    $("#weather-city").text(toTitleCase(preferences.location.replace(/,/g, " ")));
+    $("#weather-status").text(preferences.weather.text);
+    $("#weather-temp").text(preferences.weather.temperature);
+    $("#weather-icon").attr("src", preferences.weather.icon);
+    $("#weather-container").attr("title", `Powered by Weather Api, Last Refresh: ${new Date(preferences.weather.lastRefresh).toLocaleTimeString("en-GB").substring(0, 5)}`);
+}
+
+function setBackgroundValuesToHtml() {
+    $("body").css("background", `linear-gradient(to bottom, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(${preferences.background.src}) no-repeat center center fixed`);
+    $("#attribution").html(`Photo on <a href=${preferences.background.link}>${preferences.background.user}</a> on <a href=https://unsplash.com>Unsplash</a>`);
 }
 
 function setAbout() {
@@ -409,11 +440,8 @@ function setAbout() {
     $(".contact").text(about.contact);
 }
 
-function changeLanguage(value) {
-    months = languages["months" + value];
-    weekdays = languages["weekdays" + value];
-    weekdaysShort = languages["weekdaysShort" + value]
-    showWeather(value, true);
+function changeLanguage() {
+    showWeather(true);
     clearInterval(dateInterval);
     showDate();
     showCalendar(0);
@@ -422,7 +450,7 @@ function changeLanguage(value) {
 function toTitleCase(str) {
     return str.replace(
         /\w\S*/g,
-        (txt) => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
+        (txt) => txt.charAt(0).toUpperCase() + txt.substring(1).toLowerCase()
     );
 }
 
